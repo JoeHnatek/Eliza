78507c6ca6644933cfba8c0c9ea4c59351dd330f
Joseph Hnatek <24803352+jmh07@users.noreply.github.com>
2020-09-25 02:54:49 -0500

    Added additional regex

--- Eliza.py
+++ Eliza.py
@@ -69,7 +69,7 @@ if(__name__ == "__main__"):
 	"""
 
 	rules = [[r'\?',["Would that help you?", "Perhaps.", "Why are you asking me?"]],
-			[r'(^[a-zA-Z] [0-9]{2,4}$)', ["Tell me more about {0}.", "Are you handling {0} well?", "Do you want to drop {0}?", "Do you want to add {0}?", "You should really look more into {0}, it sounds fun."]],
+			[r'(^[a-zA-Z]+ [0-9]{2,4}$)', ["Tell me more about {0}.", "Are you handling {0} well?", "Do you want to drop {0}?", "Do you want to add {0}?", "You should really look more into {0}, it sounds fun."]],
 			[r'(\bI\b) (\blove\b|\bhate\b|\blike\b|\bneed(ed)?\b) (.*)',['Why do you {1} {3}?']],
 			[r'\b(my)?\b \bname\b \b(is)?\b (.*)',["Hi {2}, what are you majoring in?"]],
 			[r'(\bhello\b|\bhi\b|\bhey\b)',["Hello! How is your day?", "Hi! I am an academic advisor. How can I help?"]],
@@ -84,6 +84,7 @@ if(__name__ == "__main__"):
 			[r'\byes\b',["Great! Anything else?", "Good! I'll make a note of that.", "Great! Anything else you want to talk about?"]],
 			[r'\bno(pe)?\b', ["Why is that?", "Okay. I'll make a note of that. Anything else?"]],
 			[r'\bgrade(s)?\b', ["How are your classes going?", "Is your GPA good?"]],
+			[r'.* (\bterrible\b|\bhorrible\b)', ["Sorry to hear that it is {0}. How can I help?", "Why is it {0}", "How can I help it not be {0}?"]],
 			[r'.* \bhomework\b (.*)', ["Have you been doing well with them?", "You say the homework {1}. How so?", "Are you doing them well?"]],
 			[r'.* \bwell\b', ["What is making it go well?", "Can it get any better?", "What could go better for you?", "Well? Would you want them to be better?"]],
 			[r'\b(.*)\b (\bsound(s)?\b) (.*)', ["Why does {0} sound {3}", "Would you take it if it only sounded {3}?", "{0} does sound {3}"]], # 0 1
@@ -91,17 +92,22 @@ if(__name__ == "__main__"):
 			[r'(\bI\b)? (\bam\b)? \bstudy(ing)?\b (.*)', ["Do you enjoy {3}?", "How is {3} going?", "Is {3} required to graduate?"]],#3
 			[r'\bI\b (\bwant\b|\bneed\b) to (\badd\b|\bremove\b) \ba class(es)?\b', ["Why do you {0} to {1} a class?", "Sure what class? [ABCD 1111]"]],
 			[r'(\bGood\b|\bbad\b|\bokay\b)', ["Why is it {0}?", "{0}?", "Elaborate why it is {0}."]],
+			[r'\bare\b \byou\b (.*)', ["Why do you ask if I am {0}?", "So what if I was {0}?"]],
 			[r'\bnot really\b', ["That's okay. Would you consider doing research?", "Why not?", "Sure. You should really think about it."]],
 			[r'\binteresting\b', ["Why is it interesting?", "Tell me more about why you think it is.", "Would you enjoy it more?", "Would that help in class?"]],
-			[r'[\bgrade(s)\b?]',["Do you think it could be higher?", "Do you want to improve your grades?"]],
+			[r'.* \bgrade(s)?\b',["Do you think it could be higher?", "Do you want to improve your grades?"]],
 			[r'\bcan i ask\b', ["What do you want to ask?"]],
 			[r'\bjob(s)?\b', ["I'm sure there are jobs out there for you! I can help you look for them.", "What are you looking for?"]],
-			[r'(.*)? \bsalary\b', ["How much?", "That sounds like a solid plan!"]],
+			[r'\bsalary\b', ["How much?", "That sounds like a solid plan!"]],
 			[r'\bthanks\b', ["You're welcome! Anything else?", "No problem!"]],
 			[r'\bmilitary\b', ["Why the military?"]],
+			[r'\bdont\b (.*)', ["Okay, I won't {0}", "Hmm. I'll take that into acount."]],
+			[r'\bmaybe\b', ["You should commit more.", "Think about it more.", "Please consider it."]],
 			[r'\bgrades are\b (\bbad\b|\bgood\b|\bgreat\b|\bokay\b)', ["Do you think you can improve it more?", "{0}? Is that what you expect?"]],
 			[r'\bregister\b', ["What are you looking to register?"]],
 			[r'\bI\b \bdo\b', ["Good! How are your grades?"]],
+			[r'\bI\b \bwill\b', ["Great! How is life going?", "Thanks. Can I do anything else?"]],
+			[r'.* \btuition\b', ["Tuition is important. It helps pay for this service.", "Tuition can be costly, how can I help?"]],
 			[r"(\bI\b|\bI'm\b) \bnot\b \bsure\b", ["That's okay. You should though. What can I help with that?"]],
 			[r'.* \bhold\b', ["Sure I can help with that, what's the hold for?"]],
 			[r'.* \bowe money\b', ["First you need to pay it off. Do you need support?"]],
@@ -112,9 +118,9 @@ if(__name__ == "__main__"):
 			[r'\burop\b', ["UROP is a great way to do research! Do you want some info on that?"]],
 			[r'\bdont understand\b', ["It can be difficult. Can I do anything else for you?"]],
 			[r'\bits\b \bokay\b', ["Sure it is. What other help do you need?"]],
+			[r'.* \bthat\b \bis\b (.*)', ["Why do you say it is {0}?", "{0}? Howcome?"]],
 			[r'\bthey\b \bare\b (\bgood\b|\bbad\b)', ["That's {0} to hear.", "What else can I help with."]],
 			[r"(\bI\b|\bI'm\b) (\bam\b)? \bhope?(ing)?\b (.*)", ["Why do you hope?", "Are you doing well?", "Can you do something instead?"]],
-			[r'(^[a-zA-Z]+$)',["Hello {0}. How can I help?", "Hi there {0}! What could I help you with?"]],
 			[r"(.*)",["Can you elaborate on that?","Interesting. Tell me more.", "How come?", "I'm sorry, I'm not quite sure I understand. Can you explain.", "How are your classes going?", "Have you thought about your future at all?"]]]
 
 
